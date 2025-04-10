e4e348bfe17faa78854e863145105ddc8345450b
Nayan Das <nayan010das@gmail.com>
2025-04-10 16:57:01 +0530

    commit

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,7 +1,7 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
-import { Email } from './email';
+
 export async function sendVerificationEmail(
   email: string,
   username: string,

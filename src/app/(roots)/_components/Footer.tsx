481d08f1d2d85dd95ae4f5bd9a0bfb602eb9729a
Nayan Das <nayan010das@gmail.com>
2025-05-20 18:24:16 +0530

    UI Fixed

--- src/app/(roots)/_components/Footer.tsx
+++ src/app/(roots)/_components/Footer.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import Link from "next/link";
+
 function Footer() {
   return (
     <footer className="w-full py-4 text-center text-gray-600 text-sm font-mono">
-      Made with ❤️ by NAYAN
+      Made with ❤️ by <Link href={"https://www.linkedin.com/in/nayan-das2004/"} className="text-black">NAYAN</Link>
     </footer>
   );
 }
